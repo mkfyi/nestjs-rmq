60d036a594c3754055266ca702887a6551ed731a
mkilian95 <112434983+mkilian95@users.noreply.github.com>
2023-05-25 17:52:06 +0200

    🧱 Added abstract base queue manager class

---
+++ lib/core/managers/base.queue-manager.ts
@@ -0,0 +1,71 @@
+import { ModuleRef } from '@nestjs/core';
+import { Type } from '@nestjs/common';
+import { QueueHandler } from '../../common/interfaces/queue-handler.interface';
+import { ExceptionHandler } from '../../common/interfaces/exception-handler.interface';
+import { QueueHandlerMetadata } from '../../common/interfaces/queue-handler.metadata';
+import { Connection } from '../../common/interfaces/connection.interface';
+import { Connections } from '../connections';
+
+const HANDLER_METADATA = Symbol.for('zmq.manager.handler_metadata');
+
+export abstract class BaseQueueManager<Ret = void> {
+  private readonly [HANDLER_METADATA]: string;
+
+  protected handlers = new Map<
+    string,
+    { handler: Type<QueueHandler<Ret>>; active?: boolean }
+  >();
+
+  protected constructor(
+    protected readonly moduleRef: ModuleRef,
+    protected readonly connections: Connections,
+    protected readonly exceptionHandler: ExceptionHandler,
+    metadataKey: string,
+  ) {
+    this[HANDLER_METADATA] = metadataKey;
+  }
+
+  public async apply(): Promise<void> {
+    await Promise.all(
+      [...this.handlers.values()]
+        .filter(({ active }) => !active)
+        .map(async (entry): Promise<void> => {
+          const metadata = this.extractMetadata(
+            this[HANDLER_METADATA],
+            entry.handler,
+          );
+
+          const connection = this.connections.get(metadata.connection);
+
+          if (connection !== null && connection.connected) {
+            await this.bind(connection, entry.handler, metadata);
+          }
+        }),
+    );
+  }
+
+  public register(handlers: Type<QueueHandler<Ret>>[]): void {
+    handlers.forEach((handler) => this.registerHandler(handler));
+  }
+
+  protected abstract bind<
+    T extends QueueHandlerMetadata = QueueHandlerMetadata,
+  >(
+    connection: Connection,
+    target: Type<QueueHandler<Ret>>,
+    metadata: T,
+  ): Promise<void>;
+
+  protected registerHandler(handler: Type<QueueHandler<Ret>>): void {
+    if (!this.handlers.has(handler.name)) {
+      this.handlers.set(handler.name, { handler });
+    }
+  }
+
+  protected extractMetadata<Metadata extends QueueHandlerMetadata>(
+    metadataKey: string,
+    handler: Type<QueueHandler<Ret>>,
+  ): Required<Metadata> {
+    return Reflect.getMetadata(metadataKey, handler) as Required<Metadata>;
+  }
+}
