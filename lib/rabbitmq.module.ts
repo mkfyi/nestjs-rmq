9e6b8b6275663baa50aafff7d784ed0124f247f3
mkilian95 <112434983+mkilian95@users.noreply.github.com>
2023-05-25 19:22:42 +0200

    🚀 Managing all available queues

--- lib/rabbitmq.module.ts
+++ lib/rabbitmq.module.ts
@@ -22,15 +22,70 @@ import { buildConnectionToken } from './core/utils/build-connection-token';
 import { ConnectionWrapper } from './core/wrappers/connection.wrapper';
 import { BaseExceptionHandler } from './core/base-exception-handler';
 import { Connections } from './core/connections';
-import { QueueManagers } from './core/managers';
+import {
+  ListenerQueueManager,
+  PubSubQueueManager,
+  QueueManagers,
+  RoutingQueueManager,
+  RpcQueueManager,
+  TopicsQueueManager,
+  WorkerQueueManager,
+} from './core/managers';
+import { QueueHandlerExplorer } from './core/queue-handler-explorer';
 
 @Module({})
 export class RabbitMQModule implements OnApplicationBootstrap {
-  public constructor(private readonly connections: Connections) {}
+  public constructor(
+    private readonly connections: Connections,
+    private readonly queueHandlerExplorer: QueueHandlerExplorer,
+    private readonly listenerQueueManager: ListenerQueueManager,
+    private readonly pubSubQueueManager: PubSubQueueManager,
+    private readonly routingQueueManager: RoutingQueueManager,
+    private readonly rpcQueueManager: RpcQueueManager,
+    private readonly topicsQueueManager: TopicsQueueManager,
+    private readonly workerQueueManager: WorkerQueueManager,
+  ) {}
 
   public onApplicationBootstrap(): void {
+    const { listener, pubSub, routing, rpc, topics, worker } =
+      this.queueHandlerExplorer.explore();
+
+    this.listenerQueueManager.register(listener);
+    this.pubSubQueueManager.register(pubSub);
+    this.routingQueueManager.register(routing);
+    this.rpcQueueManager.register(rpc);
+    this.topicsQueueManager.register(topics);
+    this.workerQueueManager.register(worker);
+
+    this.connectAndApply(true);
+  }
+
+  public async applyQueueManagers(): Promise<void> {
+    await Promise.all([
+      this.listenerQueueManager.apply(),
+      this.pubSubQueueManager.apply(),
+      this.routingQueueManager.apply(),
+      this.rpcQueueManager.apply(),
+      this.topicsQueueManager.apply(),
+      this.workerQueueManager.apply(),
+    ]);
+  }
+
+  public connect(applyQueueManagers?: boolean): void {
+    this.connectAndApply(false, applyQueueManagers);
+  }
+
+  private connectAndApply(
+    autoConnect: boolean,
+    applyQueueManagers?: boolean,
+  ): void {
     this.connections
-      .connect(true)
+      .connect(autoConnect)
+      .then(async () => {
+        if (applyQueueManagers ?? true) {
+          await this.applyQueueManagers();
+        }
+      })
       .catch((e) => Logger.error(e.message, RabbitMQModule.name));
   }
 
@@ -96,6 +151,8 @@ export class RabbitMQModule implements OnApplicationBootstrap {
       global: true,
       imports,
       providers: [
+        RabbitMQModule,
+        QueueHandlerExplorer,
         exceptionHandler
           ? {
               provide: EXCEPTION_HANDLER_INJECTION_TOKEN,
@@ -108,7 +165,7 @@ export class RabbitMQModule implements OnApplicationBootstrap {
         ...QueueManagers,
         ...connections,
       ],
-      exports: connections,
+      exports: [RabbitMQModule, ...connections],
     };
 
     dynModule.providers?.push({
