cbb51796b5f73ceb6230db83e5283ad099955f5b
mkilian95 <112434983+mkilian95@users.noreply.github.com>
2023-05-26 00:49:43 +0200

    ✨ Implemented routing queue adapter

---
+++ lib/core/adapter/routing.queue-adapter.ts
@@ -0,0 +1,23 @@
+import { BaseQueueAdapter } from './base.queue-adapter';
+import { Connection } from '../../common/interfaces/connection.interface';
+// noinspection ES6PreferShortImport
+import { Channel } from '../../common/interfaces/amqp-wrapper.interfaces';
+
+export class RoutingQueueAdapter extends BaseQueueAdapter {
+  public constructor(
+    connection: Connection,
+    queue: string,
+    private readonly route: string,
+  ) {
+    super(connection, queue);
+  }
+
+  protected async sendInternal(
+    channel: Channel,
+    payload: Buffer,
+  ): Promise<boolean> {
+    await channel.assertExchange(this.queue, 'direct', { durable: false });
+
+    return channel.publish(this.queue, this.route, payload);
+  }
+}
