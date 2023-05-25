5f6a39a81923efcdfcc32641cb66d8e9877338eb
mkilian95 <112434983+mkilian95@users.noreply.github.com>
2023-05-26 00:49:34 +0200

    ✨ Implemented worker queue adapter

---
+++ lib/core/adapter/worker.queue-adapter.ts
@@ -0,0 +1,19 @@
+import { BaseQueueAdapter } from './base.queue-adapter';
+import { Connection } from '../../common/interfaces/connection.interface';
+// noinspection ES6PreferShortImport
+import { Channel } from '../../common/interfaces/amqp-wrapper.interfaces';
+
+export class WorkerQueueAdapter extends BaseQueueAdapter {
+  public constructor(connection: Connection, queue: string) {
+    super(connection, queue);
+  }
+
+  protected async sendInternal(
+    channel: Channel,
+    payload: Buffer,
+  ): Promise<boolean> {
+    await channel.assertQueue(this.queue, { durable: true });
+
+    return channel.sendToQueue(this.queue, payload, { persistent: true });
+  }
+}
