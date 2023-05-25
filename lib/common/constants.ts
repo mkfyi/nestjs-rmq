77a1da3fff65e3656d3c4da551efbdb42db612b5
mkilian95 <112434983+mkilian95@users.noreply.github.com>
2023-05-25 16:24:44 +0200

    🏷️ Updated metadata keys

--- lib/common/constants.ts
+++ lib/common/constants.ts
@@ -1,1 +1,7 @@
-export const METADATA_KEY_CONNECTION_TOKENS = 'rmq.meta.connection_tokens';
+export const LISTENER_HANDLER_METADATA = 'rmq.meta.handler.listener';
+export const PUB_SUB_ANDLER_METADATA = 'rmq.meta.handler.pub_sub';
+export const ROUTING_HANDLER_METADATA = 'rmq.meta.handler.routing';
+export const RPC_HANDLER_METADATA = 'rmq.meta.handler.rpc';
+export const TOPICS_HANDLER_METADATA = 'rmq.meta.handler.topics';
+export const WORKER_HANDLER_METADATA = 'rmq.meta.handler.worker';
+export const EXCEPTION_HANDLER_INJECTION_TOKEN = 'rmq.exception_handler';
