0dea1b405a9fd3bb9862bd0336a4afb0363cec45
mkilian95 <112434983+mkilian95@users.noreply.github.com>
2023-05-25 18:59:14 +0200

    ✨ Implemented rpc queue manager

---
+++ lib/core/managers/rpc.queue-manager.ts
@@ -0,0 +1,117 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ModuleRef } from '@nestjs/core';
+import {
+  EXCEPTION_HANDLER_INJECTION_TOKEN,
+  MESSAGE_HEADER_REPLY_TYPE,
+  RPC_HANDLER_METADATA,
+} from '../../common/constants';
+// noinspection ES6PreferShortImport
+import {
+  Channel,
+  ConsumeMessageFields,
+} from '../../common/interfaces/amqp-wrapper.interfaces';
+import { ExceptionHandler } from '../../common/interfaces/exception-handler.interface';
+import {
+  QueueHandler,
+  Reply,
+} from '../../common/interfaces/queue-handler.interface';
+import { QueueHandlerMetadata } from '../../common/interfaces/queue-handler.metadata';
+import { MessageWrapper, ReplyType } from '../wrappers/message.wrapper';
+import { Connections } from '../connections';
+import { BaseQueueManager } from './base.queue-manager';
+
+@Injectable()
+export class RpcQueueManager extends BaseQueueManager<Reply> {
+  public constructor(
+    moduleRef: ModuleRef,
+    connections: Connections,
+    @Inject(EXCEPTION_HANDLER_INJECTION_TOKEN)
+    exceptionHandler: ExceptionHandler,
+  ) {
+    super(moduleRef, connections, exceptionHandler, RPC_HANDLER_METADATA);
+  }
+
+  protected async bind(
+    channel: Channel,
+    handler: QueueHandler<Reply>,
+    metadata: QueueHandlerMetadata,
+  ): Promise<void> {
+    await channel.assertQueue(metadata.queue, { durable: false });
+    await channel.prefetch(1);
+    await channel.consume(
+      metadata.queue,
+      (msg) => {
+        if (msg !== null) {
+          const message = new MessageWrapper<ConsumeMessageFields>(msg);
+
+          handler
+            .execute(message)
+            .then((reply) => this.reply(channel, message, reply))
+            .catch((e) =>
+              this.reply(
+                channel,
+                message,
+                this.exceptionHandler.handle(e, metadata.queue),
+              ),
+            )
+            .finally(() => channel.ack(msg));
+        }
+      },
+      { noAck: true },
+    );
+  }
+
+  private reply(
+    channel: Channel,
+    msg: MessageWrapper<ConsumeMessageFields>,
+    reply: Reply,
+  ) {
+    if (reply.payload instanceof Uint8Array) {
+      channel.sendToQueue(
+        msg.getReplyTo() as string,
+        reply.payload instanceof Buffer
+          ? reply.payload
+          : Buffer.from(
+              reply.payload instanceof Uint8Array
+                ? reply.payload
+                : typeof reply.payload === 'object'
+                ? JSON.stringify(reply.payload)
+                : reply.payload != undefined
+                ? `${reply.payload}`
+                : JSON.stringify(reply),
+            ),
+        {
+          headers: this.getReplyTypeHeaders(reply),
+        },
+      );
+    }
+  }
+
+  private getReplyTypeHeaders(reply: Reply): Record<string, string> {
+    const result = {} as Record<string, string>;
+
+    if (reply.payload instanceof Uint8Array) {
+      result[MESSAGE_HEADER_REPLY_TYPE] = ReplyType.Buffer.toString();
+    } else {
+      switch (typeof reply.payload) {
+        case 'boolean':
+          result[MESSAGE_HEADER_REPLY_TYPE] = ReplyType.Bool.toString();
+          break;
+
+        case 'number':
+          result[MESSAGE_HEADER_REPLY_TYPE] = ReplyType.Number.toString();
+          break;
+
+        case 'string':
+          result[MESSAGE_HEADER_REPLY_TYPE] = ReplyType.Text.toString();
+          break;
+
+        default:
+          result[MESSAGE_HEADER_REPLY_TYPE] = ReplyType.Json.toString();
+          break;
+      }
+    }
+
+    return result;
+  }
+}
