9311def30c05fa240a5ba9dff3e61b958968dc77
mkilian95 <112434983+mkilian95@users.noreply.github.com>
2023-05-25 13:40:33 +0200

    🧱 Added a custom Connection interface

---
+++ lib/common/interfaces/connection.interface.ts
@@ -0,0 +1,32 @@
+// noinspection ES6PreferShortImport
+import { Channel } from './amqp-wrapper.interfaces';
+
+export interface Connection {
+  /**
+   * The name of the connection during module setup. If only one connection is
+   * given without passing a name, "default" will be used.
+   */
+  name: string;
+
+  /**
+   * Determines if the connection has been established.
+   */
+  connected(): boolean;
+
+  /**
+   * Connects to an AMQP server.
+   *
+   * @throws ConnectionFailedException
+   */
+  connect(): Promise<void>;
+
+  /**
+   * Closes the open connection to an AMQP server.
+   */
+  close(): Promise<void>;
+
+  /**
+   * Creates a channel for this specific connection.
+   */
+  channel(): Promise<Channel>;
+}
