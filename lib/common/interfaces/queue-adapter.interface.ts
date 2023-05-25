405141ce1b033a60e5eb67e20d922859ccb59c72
mkilian95 <112434983+mkilian95@users.noreply.github.com>
2023-05-26 00:48:55 +0200

    🏷️ Added RpcQueueAdapter type

--- lib/common/interfaces/queue-adapter.interface.ts
+++ lib/common/interfaces/queue-adapter.interface.ts
@@ -1,3 +1,7 @@
+import { Answer } from './answer.interface';
+
 export interface QueueAdapter<Ret = boolean> {
   send<T>(data: T): Promise<Ret>;
 }
+
+export type RpcQueueAdapter = QueueAdapter<Answer>;
