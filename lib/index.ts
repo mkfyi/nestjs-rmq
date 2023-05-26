b96384a31d5601ce9e74d02f1c597e9de23384d5
mkilian95 <112434983+mkilian95@users.noreply.github.com>
2023-05-26 02:22:32 +0200

    🚀 Added library entrypoint

---
+++ lib/index.ts
@@ -0,0 +1,26 @@
+/**
+ * RabbitMQ Module for NestJS
+ * Copyright (c) 2023 - Michael Kilian
+ * https://github.com/mkilian95
+ * MIT Licensed
+ */
+
+export * from './common/exceptions/connection-failed.exception';
+export * from './common/exceptions/duplicated-connection-alias.exception';
+export * from './common/exceptions/invalid-connection.exception';
+export * from './common/exceptions/not-connected.exception';
+export * from './common/exceptions/unable-to-queue-remote-procedure-call.exception';
+export * from './common/exceptions/validation.exception';
+
+export * from './common/interfaces/amqp-wrapper.interfaces';
+export * from './common/interfaces/answer.interface';
+export * from './common/interfaces/connection.interface';
+export * from './common/interfaces/exception-handler.interface';
+export * from './common/interfaces/message.interface';
+export * from './common/interfaces/queue-adapter.interface';
+export * from './common/interfaces/queue-adapter-type.enum';
+export * from './common/interfaces/queue-handler.interface';
+export * from './common/interfaces/rabbitmq.module-options';
+export * from './common/decorators';
+
+export * from './rabbitmq.module';
